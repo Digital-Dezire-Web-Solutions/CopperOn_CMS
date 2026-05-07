d153d8d62620214f28cdd0b0530275d5072068a7
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2026-05-07 13:24:08 +0530

    category updated

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -535,6 +535,8 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
